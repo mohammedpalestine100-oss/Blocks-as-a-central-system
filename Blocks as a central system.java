c606ee71df1ae2c478dd511a40087a70f940431c
mohammedpalestine100-oss <mohammed.palestine100@gmail.com>
2025-09-28 13:37:00 +0300

    Update Blocks as a central system.java

--- Blocks as a central system.java
+++ Blocks as a central system.java
@@ -1,3 +1,15 @@
+package com.creata.poa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreataApp {
+  public static void main(String[] args) {
+    SpringApplication.run(CreataApp.class, args);
+  }
+}
+
 package com.creata.poa.core;
 
 import java.security.MessageDigest;
@@ -383,3 +395,4 @@ public class BlockchainController {
     }
   }
 }
+
